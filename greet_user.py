fc86ae623a10194d62733a62baed0878829c1f04
nemoest1123 <nemoest@gmail.com>
2017-02-04 17:47:43 -0500

    Chapter 10 Work
    This is all the work from Chapter 10 in the book

---
+++ greet_user.py
@@ -0,0 +1,10 @@
+# Python Template for Geany
+
+import json
+
+filename = 'username.json'
+
+with open(filename) as f_obj:
+    username = json.load(f_obj)
+    print("Welcome back, " + username + "!")
+
